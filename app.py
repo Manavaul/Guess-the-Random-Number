bf663c1819272f2e53233c807742f7f530ecd748
Manav S Aul <manavaul@Manav-Student-BYOD.local>
2019-03-12 15:06:12 -0500

    Work in progress

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 guess = 0
 
 random_number = random.randint(1, 100)
-
+print random_number
 print (" Guess The Number Game! ")
 
 text = input (" Guess a number between 1 and 100: you have 5 guesses ")
@@ -16,9 +16,10 @@ while random_number != "text" and guess < 4:
         print " Your guess is too high! "
         text = input (" Guess a number between 1 and 100: ")
         guess = guess + 1
-    else:
-        print " You guessed the number! "
         break
-if guess <= 4:
+if guess == 4 and text != random_number:
     print " You Lose! "
-    
+    print "The random number was:"
+    print random_number
+elif guess >== 4 and text == random_number:
+    print " You guessed the number! "
