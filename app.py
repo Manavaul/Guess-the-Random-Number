2489fe421625a7cea54c9546d2fa0be57e1463ab
Manav S Aul <manavaul@Manav-Student-BYOD.local>
2019-03-13 14:38:24 -0500

    The completed and edited version of the number guessing game

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import random
 
-random_number = random.randint(1, 100)
-
 print (" Guess The Number Game! ")
+
+random_number = random.randint(1, 100)
+guess = 0
+countdown = 9
 
 text = input (" Guess a number between 1 and 100: ")
 
-while random_number != "text":
+while random_number != text and guess < 9:
     if text < random_number:
-        print " Your guess is too low! "
+        print "Your guess is too low!"
+        print ('You have {} guess[es] left'.format(countdown))
         text = input (" Guess a number between 1 and 100: ")
+        guess += 1
+        countdown -= 1
     elif text > random_number:
-        print " Your guess is too high! "
+        print " Your guess is too high!"
+        print ('You have {} guess[es] left'.format(countdown))
         text = input (" Guess a number between 1 and 100: ")
-    else:
+        guess += 1
+        countdown -= 1
+    elif guess <= 9 and text == random_number:
         print " You guessed the number! "
-        break
+
+if guess <= 9 and text == random_number:
+    print " You guessed the number! "
+elif guess == 9 and text != random_number:
+    print " You Lose! "
+    print "The random number was:"
+    print random_number
